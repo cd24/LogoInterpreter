d6c928d99e8ef29f9dc79514d4c90f773bc540f8
John McAvey <john@mcavey.net>
2015-02-19 20:10:08 -0600

    Started Keyboard Shortcuts
    Transfer Commit

--- src/parser/Controller.java
+++ src/parser/Controller.java
@@ -4,8 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Point2D;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -22,6 +26,8 @@ public class Controller implements Initializable{
     Button left;
     @FXML
     Button right;
+    @FXML
+    Button reset;
 
     @FXML
     TextField singleCommand;
@@ -34,9 +40,11 @@ public class Controller implements Initializable{
     Pane canvas;
 
     @FXML
-    ImageView turtleImage;
+    Ellipse turtleImage;
 
     Turtle turtle = Turtle.getInstance();
+
+    Thread evalThread = new Thread();
 
     void initialize() {
         System.out.println("Called");
@@ -47,9 +55,27 @@ public class Controller implements Initializable{
     public void initialize(URL location, ResourceBundle resources) {
         passedCommands.setEditable(false);
 
-        Point2D starting = turtle.updateForCanvasSized(422, 618);
-        turtleImage.setTranslateY(starting.getY());
-        turtleImage.setTranslateX(starting.getX());
+        Point2D starting = turtle.updateForCanvasSized(430, 618);
+
+        canvas.setOnKeyPressed((KeyEvent event) -> {
+            switch (event.getCode()){
+                case UP:
+                    moveForward();
+                    break;
+                case DOWN:
+                    moveBackward();
+                    break;
+                case LEFT:
+                    moveLeft();
+                    break;
+                case RIGHT:
+                    moveRight();
+                    break;
+            }
+        });
+
+        canvas.requestFocus();
+
         System.out.println("X, Y: " + starting.getX() + " , " + starting.getY());
         System.out.println("Height, Width: " + canvas.getHeight() + " , " + canvas.getWidth());
     }
@@ -70,15 +96,41 @@ public class Controller implements Initializable{
     void moveBackward() {
         this.updateTurtle(turtle.asPoint(), turtle.moveBackward(10));
     }
+    @FXML
+    void resetBoard() {
+        if (evalThread.isAlive()){
+            evalThread.interrupt();
+        }
+    }
+
     void updateTurtle(Point2D start, Point2D end){
+        createLine(start, end);
+
+        turtleImage.setTranslateY(end.getY());
+        turtleImage.setTranslateX(end.getX());
+
+        System.out.println("X, Y: " + end.getX() + ", " + end.getY());
+    }
+
+    void createLine(Point2D start, Point2D end){
         Line connector = new Line();
         connector.setStartX(start.getX());
         connector.setStartY(start.getY());
         connector.setEndX(end.getX());
         connector.setEndY(end.getY());
         canvas.getChildren().add(connector);
+    }
 
-        turtleImage.setTranslateY(end.getY());
-        turtleImage.setTranslateX(end.getX());
+    @FXML
+    void issueCommand(String command){
+        passedCommands.appendText(command + "\n");
+    }
+
+    @FXML
+    void giveSingleCommand() {
+        if (singleCommand.getText().length() > 0) {
+            issueCommand(singleCommand.getText());
+        }
+        singleCommand.setText("");
     }
 }
